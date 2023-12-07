2a311c8aee1bd20835e71ecb5f355d7d49e699c0
Cauã Tavares <tavarescauac@gmail.com>
2023-12-07 13:08:55 -0300

    refactor: refatorando codigo

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:desafio_escriboo/widgets/book_list.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: BookList(),
+      ),
+    );
+  }
+}
