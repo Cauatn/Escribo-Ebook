4fbc1fb817a8ed466d1c9b19c135743eb702a70d
Cauã Tavares <tavarescauac@gmail.com>
2023-12-07 13:07:38 -0300

    build: adicionando title, author e url

--- lib/model/book.dart
+++ lib/model/book.dart
@@ -1,17 +1,21 @@
 class Book {
   int id;
   String title;
-  //String url;
-  //String coverUrl;
+  String coverUrl;
+  String author;
 
   Book.fromJson(Map json)
       : id = json['id'],
+        coverUrl = json['cover_url'],
+        author = json['author'],
         title = json['title'];
 
   Map toJson() {
     return {
       'id': id,
       'title': title,
+      'coverUrl': coverUrl,
+      'author': author,
     };
   }
 }
